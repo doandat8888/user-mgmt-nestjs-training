62aed274dc2af6177dd19062800da7b9637d0e28
unknown <badatdoan@gmail.com>
2024-05-31 14:18:22 +0700

    [File]: Add README.md file

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
             whitelist: true //Make sure that there is no extra property in request
         })
     )
-    await app.listen(3000);
+    app.enableCors();
+    await app.listen(8000);
 }
 bootstrap();
